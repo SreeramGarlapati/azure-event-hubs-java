7a7f676e7929334a99239f4a32fe6c26a5d4d9c0
Sreeram Garlapati <Sree@Sree.local>
2017-03-05 19:33:42 -0800

    eventdata batch

---
+++ azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/EventDataBatch.java
@@ -0,0 +1,34 @@
+    /*
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+package com.microsoft.azure.eventhubs;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.qpid.proton.Proton;
+import org.apache.qpid.proton.amqp.Binary;
+import org.apache.qpid.proton.amqp.Symbol;
+import org.apache.qpid.proton.amqp.messaging.ApplicationProperties;
+import org.apache.qpid.proton.amqp.messaging.Data;
+import org.apache.qpid.proton.amqp.messaging.MessageAnnotations;
+import org.apache.qpid.proton.message.Message;
+
+import com.microsoft.azure.servicebus.amqp.AmqpConstants;
+
+/**
+ * The data structure encapsulating the Event being sent-to and received-from EventHubs.
+ * Each EventHubs partition can be visualized as a Stream of {@link EventData}.
+ */
+public class EventDataBatch {
+    
+}
