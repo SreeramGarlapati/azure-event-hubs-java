806065e5fc0c983c65e66cf63bb7ef0a7e8e2a2e
System Administrator <root@Sree.local>
2017-04-13 14:29:29 -0700

    tune the sample

--- azure-eventhubs-samples/src/main/java/com/microsoft/azure/eventhubs/samples/Benchmarks/BenchMarkIngressLatency.java
+++ azure-eventhubs-samples/src/main/java/com/microsoft/azure/eventhubs/samples/Benchmarks/BenchMarkIngressLatency.java
@@ -23,11 +23,20 @@ import java.util.function.BiConsumer;
  * Performance BenchMark is specific to customers load pattern!!
  *
  * This sample is intended to highlight various variables available to tune latencies
- * - One variable that cannot be exercised in Code - is Network proximity
- *  (make sure to run in same region/AzureDataCenter as your target scenario - to get identical results)
+ *
+ * Two variables that cannot be exercised in the below Code - are
+ * 1) Network proximity
+ *      (make sure to run in same region/AzureDataCenter as your target scenario - to get identical results)
+ * 2) Throughput units configured on the EventHubs namespace
+ *      Latency numbers can only be measured when the target load is <= allowed load
+ *      if the Target load (no. of messages per sec or no. of bytes transferred per sec) exceeds the Throughput unit configuration
+ *      then you might see a long tail in 999'ile of ~4 secs - which service intentionally delays to slow down the sender
  *
  * If you are running this against an EventHubs Namespace in a shared public instance
- * - results might slightly vary across runs; if you want more predictable results - use Dedicated EventHubs .
+ * - results might slightly vary across runs; if you want more predictable results - use Dedicated EventHubs.
+ *
+ * This program generates 10000 samples of "latency per send call in milliseconds"
+ * After the completion of this program - copy the output to an excel sheet & crunch the numbers (like avg, %'s etc)
  */
 public class BenchMarkIngressLatency {
 
@@ -48,9 +57,9 @@ public class BenchMarkIngressLatency {
         // 3 - NO OF EVENTS - CLIENTS CAN BATCH & SEND <-- and there by optimize on ACKs returned from the Service (typically, this number is supposed to help bring 2 down)
         // 4 - NO OF SENDERS PER CONNECTION <-- This sample doesn't include this / only demonstrates what can be achieved using 1 Sender AMQP link using 1 Connection
         // ***************************************************************************************************************
-        final int EVENT_SIZE = 2048; // 2k
-        final int NO_OF_CONCURRENT_SENDS = 20;
-        final int BATCH_SIZE = 50;
+        final int EVENT_SIZE = 1024; // 1 kb
+        final int NO_OF_CONCURRENT_SENDS = 10;
+        final int BATCH_SIZE = 10;
 
 
         // Consider creating a pool of EventHubClient objects - based on the predicted load per process and this Benchmark test outcome
@@ -59,7 +68,7 @@ public class BenchMarkIngressLatency {
         final EventHubClient ehClient = EventHubClient.createFromConnectionStringSync(connStr.toString());
 
 
-        for (int dataSetCount = 0; dataSetCount < 10000; dataSetCount++) {
+        for (int dataSetCount = 0; dataSetCount < 1000; dataSetCount++) {
             final List<EventData> eventDataList = new LinkedList<>();
 
             for (int batchSize = 0; batchSize < BATCH_SIZE; batchSize++) {
@@ -75,7 +84,7 @@ public class BenchMarkIngressLatency {
                 sendTasks[concurrentSends] = ehClient.send(eventDataList).whenComplete(new BiConsumer<Void, Throwable>() {
                     @Override
                     public void accept(Void aVoid, Throwable throwable) {
-                        System.out.println(String.format("%s,%s,%s", throwable == null ? "success" : "failure", Duration.between(Instant.now(), beforeSend).toMillis()));
+                        System.out.println(String.format("%s,%s", throwable == null ? "success" : "failure", Duration.between(beforeSend, Instant.now()).toMillis()));
                     }
                 });
             }
@@ -83,5 +92,7 @@ public class BenchMarkIngressLatency {
             // wait for the first send to return - to control the send-pipe line speed & degree of parallelism
             CompletableFuture.anyOf(sendTasks).get();
         }
+
+        ehClient.closeSync();
     }
 }
