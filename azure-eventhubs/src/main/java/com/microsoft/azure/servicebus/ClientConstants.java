7a9320f68b972a02805e58182f70a00ba58c2b42
Sreeram Garlapati <SreeramGarlapati@users.noreply.github.com>
2017-06-22 12:14:15 -0700

    reduce IO Timeout

--- azure-eventhubs/src/main/java/com/microsoft/azure/servicebus/ClientConstants.java
+++ azure-eventhubs/src/main/java/com/microsoft/azure/servicebus/ClientConstants.java
@@ -45,7 +45,7 @@ public final class ClientConstants {
 
     public final static int SESSION_OPEN_TIMEOUT_IN_MS = 15000;
 
-    public final static int REACTOR_IO_POLL_TIMEOUT = 20;
+    public final static int REACTOR_IO_POLL_TIMEOUT = 2;
     public final static int SERVER_BUSY_BASE_SLEEP_TIME_IN_SECS = 4;
 
     public final static String NO_RETRY = "NoRetry";
